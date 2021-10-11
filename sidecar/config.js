eef99b9e0e3bcfcaed394fa45f784d9d2d73f628
Mike <mcmiller905@gmail.com>
2021-10-10 21:09:59 -0400

    add config.js before adding to gitignore

---
+++ sidecar/config.js
@@ -0,0 +1,7 @@
+// config.js
+// =========
+module.exports = {
+    get_client_id: function(){ return 'put-your-client-id-here'; },
+    get_client_secret: function(){ return 'put-your-client-secret-here'; },
+    get_redirect_uri: function () { return 'put-your-redirect-uri-here'; }
+};
